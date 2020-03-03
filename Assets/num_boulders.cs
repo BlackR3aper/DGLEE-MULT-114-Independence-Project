9606ea92cfa18987c329777e344ee0462be8b8d1
BlackR3aper <60709462+BlackR3aper@users.noreply.github.com>
2020-03-02 18:01:26 -0800

    Update

---
+++ Assets/num_boulders.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class num_boulders : MonoBehaviour
+{
+    public float dropspeed = 8.0f;
+    public GameObject boulders;
+
+
+    void Update()
+    {
+
+       
+    
+
+    }
+}
